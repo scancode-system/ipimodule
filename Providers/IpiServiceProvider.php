d4f0eaaf8d9fa326a4acd330e6655eece8b25d55
Your Name <you@example.com>
2020-06-14 16:32:03 -0300

    .

--- Providers/IpiServiceProvider.php
+++ Providers/IpiServiceProvider.php
@@ -10,6 +10,15 @@ use Illuminate\Support\Facades\Event;
 class IpiServiceProvider extends ServiceProvider
 {
     /**
+     * @var string $moduleName
+     */
+    protected $moduleName = 'Ipi';
+
+    /**
+     * @var string $moduleNameLower
+     */
+    protected $moduleNameLower = 'ipi';
+    /**
      * Boot the application events.
      *
      * @return void
@@ -39,7 +48,17 @@ class IpiServiceProvider extends ServiceProvider
      */
     public function registerViews()
     {
-        $viewPath = resource_path('views/modules/ipi');
+        $viewPath = resource_path('views/modules/' . $this->moduleNameLower);
+
+        $sourcePath = module_path($this->moduleName, 'Resources/views');
+
+        $this->publishes([
+            $sourcePath => $viewPath
+        ], ['views', $this->moduleNameLower . '-module-views']);
+
+        $this->loadViewsFrom(array_merge($this->getPublishableViewPaths(), [$sourcePath]), $this->moduleNameLower);
+
+        /*$viewPath = resource_path('views/modules/ipi');
 
         $sourcePath = __DIR__.'/../Resources/views';
 
@@ -49,7 +68,7 @@ class IpiServiceProvider extends ServiceProvider
 
         $this->loadViewsFrom(array_merge(array_map(function ($path) {
             return $path . '/modules/ipi';
-        }, \Config::get('view.paths')), [$sourcePath]), 'ipi');
+        }, \Config::get('view.paths')), [$sourcePath]), 'ipi');*/
     }
 
 
@@ -62,5 +81,16 @@ class IpiServiceProvider extends ServiceProvider
     public function provides()
     {
         return [];
+    }
+
+    private function getPublishableViewPaths(): array
+    {
+        $paths = [];
+        foreach (\Config::get('view.paths') as $path) {
+            if (is_dir($path . '/modules/' . $this->moduleNameLower)) {
+                $paths[] = $path . '/modules/' . $this->moduleNameLower;
+            }
+        }
+        return $paths;
     }
 }
